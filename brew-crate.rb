135cd980a5f9139116453d64d14c1f5765432dec
skim <skim@swainslane.co>
2020-05-08 10:58:44 -0700

    updates tap formula

--- brew-crate.rb
+++ brew-crate.rb
@@ -1,8 +1,8 @@
 class BrewCrate < Formula
   desc "brew command to install Rust crates"
   homepage "https://github.com/sl4m/homebrew-rust-crate"
-  url "https://github.com/sl4m/homebrew-rust-crate/archive/v0.0.1.tar.gz"
-  sha256 ""
+  url "https://github.com/sl4m/homebrew-rust-crate/archive/0.0.1.tar.gz"
+  sha256 "9ca6098d9941a7539fe8293b64882dd0485863253bcefa1f0586da2f1d457ce4"
   head "https://github.com/sl4m/homebrew-rust-crate.git"
 
   depends_on "rust"
